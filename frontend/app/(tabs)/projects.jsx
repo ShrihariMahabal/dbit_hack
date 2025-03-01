699291b3b794fe64fbf6e62d20bfb5fe191c92e1
Shrihari Mahabal <shrihari.mahabal22@spit.ac.in>
2025-03-01 15:12:47 +0530

    home page changes

--- frontend/app/(tabs)/projects.jsx
+++ frontend/app/(tabs)/projects.jsx
@@ -70,7 +70,7 @@ const ProjectList = () => {
   useEffect(() => {
     const fetchProjects = async () => {
       try {
-        const response = await fetch("http://localhost:5000/login/allprojects");
+        const response = await fetch("http://localhost:8000/login/allprojects");
         if (!response.ok) {
           throw new Error("Failed to fetch projects");
         }
