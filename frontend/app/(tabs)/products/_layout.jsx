e5eb0d377d4f0385469c1d29ef2ac5bfc5b865d3
Tanvi <tanvipatil245@gmail.com>
2025-03-01 19:40:03 +0530

    e-commerce-page-update

---
+++ frontend/app/(tabs)/products/_layout.jsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+
+export default function TravelLayout() {
+    return (
+        <Stack>
+            <Stack.Screen 
+                name="index" 
+                options={{
+               
+                    headerTitle: 'Where To?',
+                    headerTitleAlign: 'center',
+                    headerStyle: {
+                        height: 80,
+                        backgroundColor: "#0a0f1a"
+                      },
+                      headerTitleStyle: {
+                        fontSize: 18,
+                        fontWeight: "bold",
+                        color: "#FFFFFF"
+                    },
+                }} 
+            />
+        </Stack>
+    );
+}
